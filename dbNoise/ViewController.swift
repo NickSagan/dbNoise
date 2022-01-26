789622391bc8a40ba80f7b3cc3f2aaf090949155
NickSagan <9952728@gmail.com>
2022-01-26 13:58:54 +0300

    install pod 'SwiftyOnboard' conform to SwiftyOnboard protocol

--- dbNoise/ViewController.swift
+++ dbNoise/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import SwiftyOnboard
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let swiftyOnboard = SwiftyOnboard(frame: view.frame)
+        view.addSubview(swiftyOnboard)
+        swiftyOnboard.dataSource = self
     }
 
 
+}
+
+//MARK: - SwiftyOnboard protocol
+
+extension ViewController: SwiftyOnboardDataSource {
+
+    func swiftyOnboardNumberOfPages(_ swiftyOnboard: SwiftyOnboard) -> Int {
+            return 3
+        }
+
+    func swiftyOnboardPageForIndex(_ swiftyOnboard: SwiftyOnboard, index: Int) -> SwiftyOnboardPage? {
+            let page = SwiftyOnboardPage()
+            return page
+        }
 }
 
