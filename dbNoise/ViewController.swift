f9670579d4ddf66a87ad6436c7be3023cc09e1e9
NickSagan <9952728@gmail.com>
2022-01-26 13:25:10 +0300

    Initial Commit

---
+++ dbNoise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dbNoise
+//
+//  Created by Nick Sagan on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
