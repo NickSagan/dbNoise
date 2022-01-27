4101e44569dda675669c259033032192aa0296ff
NickSagan <9952728@gmail.com>
2022-01-27 17:00:58 +0300

    IBOutlets + IBActions for NoiseDetectorVC

--- dbNoise/NoiseDetectorViewController.swift
+++ dbNoise/NoiseDetectorViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class NoiseDetectorViewController: UIViewController {
-
+    
+    @IBOutlet weak var dbResultLabel: UILabel!
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var avgLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var textExplanationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func recordPressed(_ sender: UIButton) {
+        print("record")
+    }
+    
+    @IBAction func settingsPressed(_ sender: UIButton) {
+        print("settings")
+    }
+    
+    @IBAction func proPressed(_ sender: UIButton) {
+        print("pro")
+    }
+    
+    @IBAction func hearingPressed(_ sender: UIButton) {
+        print("hearing")
+    }
+    
+    @IBAction func menuPressed(_ sender: UIButton) {
+        print("menu")
+    }
 }
