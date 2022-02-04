deca4181f873593830876cb67c7c6fa1db2d6604
NickSagan <9952728@gmail.com>
2022-02-04 18:18:30 +0300

    add struct Result

---
+++ dbNoise/Models/Result.swift
@@ -0,0 +1,18 @@
+//
+//  Result.swift
+//  dbNoise
+//
+//  Created by Nick Sagan on 04.02.2022.
+//
+
+import Foundation
+
+struct Result: Codable {
+    
+    let name: String
+    let date: Date
+    let left: Int
+    let right: Int
+    
+}
+
