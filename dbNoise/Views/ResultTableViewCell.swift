9d2fbdb85f99ad50ca8ea9df7b910b191df003d4
NickSagan <9952728@gmail.com>
2022-02-05 18:23:09 +0300

    add class ResultTableViewCell

--- dbNoise/Views/ResultTableViewCell.swift
+++ dbNoise/Views/ResultTableViewCell.swift
@@ -10,20 +10,41 @@ import UIKit
 class ResultTableViewCell: UITableViewCell {
     
     @IBOutlet weak var date: UILabel!
-    
     @IBOutlet weak var right: UILabel!
-    
     @IBOutlet weak var left: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        right.translatesAutoresizingMaskIntoConstraints = false
+        left.translatesAutoresizingMaskIntoConstraints = false
+        date.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(right)
+        contentView.addSubview(left)
+        contentView.addSubview(date)
+        
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //    override func awakeFromNib() {
+    //        super.awakeFromNib()
+    //        // Initialization code
+    //
+    //        right.translatesAutoresizingMaskIntoConstraints = false
+    //        left.translatesAutoresizingMaskIntoConstraints = false
+    //        date.translatesAutoresizingMaskIntoConstraints = false
+    //
+    //        contentView.addSubview(right)
+    //        contentView.addSubview(left)
+    //        contentView.addSubview(date)
+    //    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
