ddf7c7785964d565e903c564fa6d39acdde9956c
NickSagan <9952728@gmail.com>
2022-02-11 16:41:54 +0300

    add NoiseResult struct

---
+++ dbNoise/Models/NoiseResult.swift
@@ -0,0 +1,15 @@
+//
+//  NoiseResult.swift
+//  dbNoise
+//
+//  Created by Nick Sagan on 11.02.2022.
+//
+
+import Foundation
+
+struct NoiseResult: Codable {
+    let date: String
+    let min: Int
+    let avg: Int
+    let max: Int
+}
